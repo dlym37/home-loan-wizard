4dffba786289dadd0e67fe16a78837011c968bb1
Dano Lym <dlym37@gmail.com>
2017-12-18 18:41:54 -0700

    project complete, but not keeping values at end

--- src/components/WizardTen/WizardTen.js
+++ src/components/WizardTen/WizardTen.js
@@ -1,6 +1,11 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import {connect} from 'react-redux';
+import {updateEmail} from '../../ducks/reducer';
+import {updateFirstName} from '../../ducks/reducer';
+import {updateLastName} from '../../ducks/reducer';
+
 class WizardTen extends Component {
     render(){
         return(
@@ -9,9 +14,9 @@ class WizardTen extends Component {
                     
                     <p>What is your name?</p> <br />
 
-                    <input type="text" placeholder="First Name" onChange={this.props.updateFirst}/>
-                    <input type="text" placeholder="Last Name" onChange= {this.props.updateLast}/>
-                    <input type="text" placeholder="email" onChange={this.props.updateEmail}/>
+                    <input type="text" placeholder="First Name" onChange={(e) => this.props.updateFirstName(e.target.value)}/>
+                    <input type="text" placeholder="Last Name" onChange= {(e) => this.props.updateLastName(e.target.value)}/>
+                    <input type="text" placeholder="email" onChange={(e)=> this.props.updateEmail(e.target.value)}/>
                     
                     <Link to="/wEleven"><button className="margin-btn"> Next </button></Link>
                 </div>
@@ -19,4 +24,14 @@ class WizardTen extends Component {
         )
     }
 }
-export default WizardTen;
+
+function mapStateToProps(state){
+    const {firstName, lastName, email} = state;
+    return {
+        firstName,
+        lastName,
+        email
+    }
+}
+
+export default connect(mapStateToProps, {updateFirstName, updateLastName, updateEmail})(WizardTen);
--- src/components/WizardTwo/WizardTwo.js
+++ src/components/WizardTwo/WizardTwo.js
@@ -1,21 +1,30 @@
-import React,  { Component } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+import {updateCity} from '../../ducks/reducer';
+
 class WizardTwo extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <div className="parent-div">
                 <div className="vert-align">
-            
-                <p>In what city will the property be located?</p><br />
-                    
-                        <input placeholder="city name" type="text" onChange={this.props.updateCity}/>
-                
+
+                    <p>In what city will the property be located?</p><br />
+
+                    <input placeholder="city name" type="text" onChange={(e) => this.props.updateCity(e.target.value)} />
+
                     <Link to="/wThree"><button className="wTwo-btn"> Next </button></Link>
                 </div>
             </div>
         )
     }
 }
+function mapStateToProps(state) {
+    const { city } = state;
+    return {
+        city
+    }
+}
 
-export default WizardTwo;
+export default connect(mapStateToProps, { updateCity })(WizardTwo);
